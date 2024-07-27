e106d65dc098b2da470507a0bb7c1bd84841aa81
Hansol Lee <hansollee333@gmail.com>
2024-07-27 17:27:16 +0900

    ESP8266 Practice_SQL

---
+++ IoT_ESP8266_DHT11.sql
@@ -0,0 +1,8 @@
+SHOW databases;
+# create user 'user1'@'%' identified by '1234';
+# GRANT ALL ON *.* TO 'user1'@'%';
+# create database dht11;
+use dht11;
+# SHOW tables;
+# create table dev01(id int not null auto_increment primary key,Temperature varchar(10) not null,Humidity varchar(10) not null,time timestamp default CURRENT_TIMESTAMP);
+select * from dev01;

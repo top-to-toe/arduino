4567e5a47b369d55db653543c7aa48ece61e8dbe
Hansol Lee <hansollee333@gmail.com>
2024-07-27 17:19:51 +0900

    ESP8266 practice

--- basic_insert_esp8266/basic_insert_esp8266.ino
+++ basic_insert_esp8266/basic_insert_esp8266.ino
@@ -40,6 +40,7 @@
 #include "DHT.h"                        // DHT 센서 라이브러리
 
 #define DHTPIN 5                        // DHT 센서가 연결된 핀 번호 (GPIO5) - 실습 당시 DHT 센서의 out핀이 ESP8266의 D1(GPIO5)에 연결, -는 접지(GND)에 연결하고 +는 3V3에 연결.
+                                        // 핀 연결 관련 참고 사항 구글 검색 - nodemcu gpio
 #define typeDHT DHT11                   // 사용하는 DHT 센서의 종류 (DHT11)
 
 DHT dht(DHTPIN, typeDHT);               // DHT 객체를 생성, 핀 번호와 센서 타입 설정

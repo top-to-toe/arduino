36f7a989d73e2149775cdb3340594b038fec6c50
Hansol Lee <hansollee333@gmail.com>
2024-08-03 10:49:48 +0900

    　

--- mysql/app.js
+++ mysql/app.js
@@ -13,41 +13,34 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();  
+var app = express();
 
 // connect database
 var mySqlClient = mysql.createConnection({
-	user:'user1',
-	password:'1234',
-	database:'dht11'
+	user: 'user1',
+	password: '1234',
+	database: 'dht11'
 });
 
 // generate & excute express web server
 var app = express();
-http.createServer(app).listen(8000,function(){
+http.createServer(app).listen(8000, function () {
 	console.log('Server running at http://127.0.0.1:8000');
 });
 
 // http://localhost:8000/ ---- list.html
-app.get('/',function(req,res)
-{
-	fs.readFile('list.html','utf8',function(error,data)
-	{
-		if(error)
-		{
+app.get('/', function (req, res) {
+	fs.readFile('list.html', 'utf8', function (error, data) {
+		if (error) {
 			console.log('readFile Error');
 		}
-		else
-		{
-			mySqlClient.query('select * from dev01', function(error,results)
-			{
-				if(error)
-				{
+		else {
+			mySqlClient.query('select * from dev01', function (error, results) {
+				if (error) {
 					console.log('error: ', error.message);
 				}
-				else
-				{
-					res.send(ejs.render(data,{ prodList:results }));
+				else {
+					res.send(ejs.render(data, { prodList: results }));
 				}
 			});
 		}
@@ -55,54 +48,41 @@ app.get('/',function(req,res)
 });
 
 // http://locakhost:8000/insert ------ insert.html 
-app.get('/insert',function(req,res)
-{
-	fs.readFile('insert.html','utf8',function(error,data)
-	{
-		if(error)
-		{
+app.get('/insert', function (req, res) {
+	fs.readFile('insert.html', 'utf8', function (error, data) {
+		if (error) {
 			console.log('read file error');
 		}
-		else
-		{
+		else {
 			res.send(data);
 		}
 	})
 });
 
 // http://locakhost:8000/edit/3 ---- edit.html
-app.get('/edit/:id',function(req,res)
-{
-	fs.readFile('edit.html','utf8',function(error,data)
-	{
+app.get('/edit/:id', function (req, res) {
+	fs.readFile('edit.html', 'utf8', function (error, data) {
 		mySqlClient.query('select * from dev01 where id = ?',
-		[req.params.id], function(error,result)
-		{
-			if(error)
-			{
+			[req.params.id], function (error, result) {
+			if (error) {
 				console.log('read file error');
 			}
-			else
-			{
-				res.send(ejs.render(data,{ product:result[0] }));
+			else {
+				res.send(ejs.render(data, { product: result[0] }));
 			}
 		});
 	});
 });
 
 // http://localhost:8000/delete/3 
-app.get('/delete/:id',function(req,res)
-{
+app.get('/delete/:id', function (req, res) {
 	mySqlClient.query('delete from dev01 where id = ?',
-	[req.params.id],function(error,result)
-	{
-		if(error)
-		{
+		[req.params.id], function (error, result) {
+		if (error) {
 			console.log('delete error');
 		}
-		else
-		{
-			console.log('delete id = %d',req.params.id);
+		else {
+			console.log('delete id = %d', req.params.id);
 			res.redirect('/');	// return main
 		}
 	});
@@ -111,42 +91,34 @@ app.get('/delete/:id',function(req,res)
 // handle parameter transfered by POST method
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post( '/insert', function(req, res)
-{
+app.post('/insert', function (req, res) {
 	var body = req.body;
-	
-	mySqlClient.query( 'insert into dev01(Temperature, Humidity) values(?,?,?)',
-	[body.Temperature, body.Humidity],function(error,result)
-	{
-		if(error)
-		{
+
+	mySqlClient.query('insert into dev01(Temperature, Humidity) values(?,?,?)',
+		[body.Temperature, body.Humidity], function (error, result) {
+		if (error) {
 			console.log('insert error: ', error.message);
 		}
-		else
-		{
+		else {
 			res.redirect('/');
 		}
 	});
 });
 
-app.post( '/edit/:id', function(req, res)
-{
+app.post('/edit/:id', function (req, res) {
 	var body = req.body;
-	
-	mySqlClient.query( 'update dev01 set Temperature=?, Humidity=? where id=?',
-	[body.Temperature,body.Humidity,body.id],function(error,result)
-	{
-		if(error)
-		{
+
+	mySqlClient.query('update dev01 set Temperature=?, Humidity=? where id=?',
+		[body.Temperature, body.Humidity, body.id], function (error, result) {
+		if (error) {
 			console.log('update error: ', error.message);
 		}
-		else
-		{
+		else {
 			res.redirect('/');
 		}
 	});
 });
-		
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -163,10 +135,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -174,24 +146,32 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
 module.exports = app;
+
+
+// 저번 시간에 데이터베이스(dht11) 및 테이블(dev01) 생성, esp8266 보드와 dht11 센서를 통해 얻은 온/습도 데이터 추가의 과정에 관련된 웹 페이지 제작
+// 수정 원본 파일 경로 - https://www.dropbox.com/scl/fo/sh05nl33unxil1bodb1mc/AJE8wRlpI8yu1agTkurYVgU/work/nodejs?rlkey=hxq3t8pivi9w75o1j59d4atfc&subfolder_nav_tracking=1&dl=0
+
+// Nodejs prompt
+// express mysql --ejs		// Embedded JavaScript Template 약자의 옵션 추가 입력
+// 패키지 생성 후 자동 제공되는 입력 명령 수행
